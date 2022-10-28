82fe89564de1888abf8e83a2a67bf67e7a0b1ddf
WillSanchezO <awilliam.sanchez@gmail.com>
2022-10-27 19:12:54 -0500

    Mi primer Commit
    Segunda practica de William

---
+++ app/src/main/java/com/williamsanchezospina/primeraclase/MainActivity.java
@@ -0,0 +1,27 @@
+package com.williamsanchezospina.primeraclase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+TextView tvTitulo;
+TextView tvSubtitulo;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTitulo = findViewById(R.id.tvTitulo);
+        tvTitulo.setText("Primera Clase");
+
+        tvSubtitulo = findViewById(R.id.tvSubtitulo);
+        tvSubtitulo.setText("Para la Segunda Clase Cambiar RAM de la PC");
+
+    }
+}
