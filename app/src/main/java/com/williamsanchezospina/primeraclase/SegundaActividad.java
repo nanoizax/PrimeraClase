83f3701eb3d52e1991d95ded4451194a8b980887
WillSanchezO <awilliam.sanchez@gmail.com>
2022-11-01 21:49:59 -0500

    Se creó hoja acuerdo las intrucciones brindadas, se insertó titulo, una imagen local, una imagen de la web y el botón de volver,. cuando se crea una variable, tambien debe declararse.
    Se cambio el orden del codigo para poder compilar, pasar de T a F en ACTIVITY.
    
    Para cargar imagen web se debe abrir la img en una ventana nueva para traer la url correcta.

--- app/src/main/java/com/williamsanchezospina/primeraclase/SegundaActividad.java
+++ app/src/main/java/com/williamsanchezospina/primeraclase/SegundaActividad.java
@@ -12,6 +12,7 @@ public class SegundaActividad extends AppCompatActivity {
 
     TextView tvTitulo1;
     ImageView ivcondor;
+    ImageView ivPrueba;
 
 
     @Override
@@ -22,7 +23,9 @@ public class SegundaActividad extends AppCompatActivity {
         tvTitulo1 = findViewById(R.id.tvTitulo1);
         tvTitulo1.setText("Imagenes del Condor Herido");
 
+        ivPrueba = findViewById(R.id.ivPrueba);
+
         ivcondor = findViewById(R.id.ivCondor);
-        Glide.with(this).load("https://www.google.com/search?q=pilgoso&oq=pilgoso&aqs=chrome..69i57j46i10i433i512j0i10i512l6.2703j0j1&client=ms-android-americamovil-co-revc&sourceid=chrome-mobile&ie=UTF-8#imgrc=lt7rDf3oAe6A1M").into(ivcondor);
+        Glide.with(this).load("https://www.ecured.cu/images/thumb/7/7f/Lindo_Pulgoso.png/260px-Lindo_Pulgoso.png").into(ivPrueba);
     }
 }
